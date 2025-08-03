4bfbcdd2c17fd7fef21f369b05a2b1d55bb99c60
Haizhou Ge <1352674740@qq.com>
2025-08-04 01:12:49 +0800

    add test data to check the same point number for all images and low_dim data

--- test_data.py
+++ test_data.py
@@ -1,14 +1,30 @@
 import pickle
 from rlbench.backend.observation import Observation
+from pathlib import Path
 
+tasks_dir = Path("data/GT_12T_data/train_12_GT")
+print(f"Tasks: {list(tasks_dir.iterdir())}")
 
-data_path = "/home/ghz/Work/Research/manipulate-anything/data/GT_12T_data/train_12_GT/close_box/all_variations/episodes/episode2/low_dim_obs.pkl"
-
-
-with open(data_path, "rb") as f:
-    low_dim_obs: list[Observation] = pickle.load(f)
-
-print(len(low_dim_obs))
-print(low_dim_obs[0])
-print(low_dim_obs[0].joint_positions)
-print(low_dim_obs[0].gripper_pose)
+for task in tasks_dir.iterdir():
+    print(task.name)
+    episodes_dir = (
+        "data/GT_12T_data/train_12_GT/{task_name}/all_variations/episodes/".format(
+            task_name=task.name
+        )
+    )
+    for episode in Path(episodes_dir).iterdir():
+        print(episode.name)
+        data_path = "{episode}/low_dim_obs.pkl".format(episode=episode)
+        with open(data_path, "rb") as f:
+            low_dim_obs: list[Observation] = pickle.load(f)
+            low_dim_length = len(low_dim_obs)
+            print(low_dim_length)
+            # print(low_dim_obs[0])
+            # print(low_dim_obs[0].joint_positions)
+            # print(low_dim_obs[0].gripper_pose)
+        for image_folder in episode.iterdir():
+            print(image_folder.name)
+            if image_folder.is_dir():
+                assert len(list(image_folder.iterdir())) == low_dim_length, (
+                    f"Image folder {image_folder} does not match low_dim_obs length {low_dim_length}"
+                )
